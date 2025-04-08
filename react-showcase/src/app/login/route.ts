3d591214bcd1f483fef7e344578132c86816122e
Timur-Is <t_faktiv_t@mail.ru>
2025-04-08 08:15:55 +0500

    feat: сделал автоматический переход на страницу после регистрации

--- react-showcase/src/app/login/route.ts
+++ react-showcase/src/app/login/route.ts
@@ -1,13 +1,21 @@
+import { redirect } from "next/navigation";
+
 export async function POST(request: Request) {
     const formData = await request.formData();
     const email = formData.get("email");
     const password = formData.get("password");
 
-    return await fetch("http://api:42424/reset-password", {
+    const response = await fetch("http://api:42424/reset-password", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
         },
         body: JSON.stringify({ email, password }),
     });
+
+    if (response.ok) {
+        redirect("/homepage");
+    }
+
+    return response.json();
 }
