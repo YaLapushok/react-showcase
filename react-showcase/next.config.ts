10684fb6d6aadd03b9bcfccd921e565e673001ac
Timur-Is <t_faktiv_t@mail.ru>
2025-04-15 02:41:55 +0500

    refactor: убрал неиспользованный `rewrite` (Next.js)

--- react-showcase/next.config.ts
+++ react-showcase/next.config.ts
@@ -7,10 +7,6 @@ const nextConfig: NextConfig = {
                 source: "/api/v0/:path*",
                 destination: "http://api:42424/api/v0/:path*",
             },
-            {
-                source: "/:path(confirm_email)*",
-                destination: "http://api:42424/:path*",
-            },
         ];
     }
 };
